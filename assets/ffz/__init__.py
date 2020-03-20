f682f7869eadf3def27d9b60bbdb0bbe7c819b23
uKaigo <37600692+uKaigo@users.noreply.github.com>
2020-03-20 19:58:57 -0300

    Adicionar a api

---
+++ assets/ffz/__init__.py
@@ -0,0 +1,64 @@
+from requests import get
+from datetime import datetime
+from bs4 import BeautifulSoup
+from aiohttp import ClientSession
+from .utils import Emote
+
+def _get_emotes(txt, emote, limit):
+        soup = BeautifulSoup(txt, 'html.parser')
+        if not emote:
+            emotes = soup.find_all(class_='thumbnail')
+            emote_list = []
+            for k, emote in enumerate(emotes):
+                print(k == limit)
+                if k == limit:
+                    return emote_list
+                em = Emote(int(emote.attrs['href'].split('/')[-1].split('-')[0]))
+                emote_list.append(em)
+        else:
+            aux = soup.find(id='emote-form')
+            aux = aux.find('tbody')
+            emotes = aux.find_all('tr')
+            emote_list = []
+            for _emote in emotes:
+                if not len(_emote.find_all('td')) > 1:
+                    return []
+                em_link = _emote.find('a')
+                if not em_link.text.lower() == emote.lower():
+                    continue
+                em = Emote(int(em_link.attrs['href'].split('/')[-1].split('-')[0]))
+                emote_list.append(em)
+                return emote_list
+            
+class FrankerFaceZ:
+    def __init__(self):
+        self.base = 'https://frankerfacez.com'
+
+    def wall(self, limit=None):
+        if limit:
+            if not isinstance(limit, int):
+                raise ValueError(f"'limit' should be 'int'. (received '{type(limit).__name__}' instead)")
+
+        response = get(f'{self.base}/emoticons/wall')
+        txt = response.text
+        return _get_emotes(txt, False, limit)
+
+    def search(self, query, sort='count-desc'):
+        author = False
+        if '.' in query:
+            author, query = query.split('.')
+        if not author:
+            response = get(f'{self.base}/emoticons/wall?q={query}&sort={sort}')
+            emote = False
+        else:
+            emote = query
+            response = get(f'https://www.frankerfacez.com/{author}/submissions')
+            if not response.status_code == 200:
+                return None
+        txt = response.text
+        try:
+            return _get_emotes(txt, emote, 1)[0]
+        except Exception as e:
+            print(e)
+            return None
+    
