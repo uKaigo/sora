e352936f21d2cca19833ed89c52c335e4274f153
Obi Wan <45069999+sadobiwan@users.noreply.github.com>
2020-04-07 16:26:33 +0000

    reaction_role-beta 1.0

--- cogs/events/discord_events.py
+++ cogs/events/discord_events.py
@@ -16,5 +16,28 @@ class DiscordEvents(commands.Cog, name=''):
             pass
         await self.bot.process_commands(after)
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        guild = self.bot.get_guild(675889958262931488)
+        channel = discord.utils.get(guild.channels, id=676516685192364042)
+
+        if channel is None:
+            return
+        if not payload.channel_id == 676516685192364042:
+            return
+        if payload.channel_id == None:
+            return
+        
+        if payload.user_id in self.cooldown:
+            return
+
+        if '<:sora_online:677666240524976128>'  == str(payload.emoji):
+                guild = self.bot.get_guild(payload.guild_id)
+                cargo = guild.get_role(688526866121883652)
+                membro = guild.get_member(payload.user_id)
+                if cargo not in membro.roles:
+                    await membro.add_roles(cargo)
+                break
+
 def setup(bot):
     bot.add_cog(DiscordEvents(bot))
