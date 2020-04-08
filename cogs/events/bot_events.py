7ed904f87968ecf740e822277c31c2aed9eca803
uKaigo <37600692+uKaigo@users.noreply.github.com>
2020-04-07 21:38:31 -0300

    Log Guilds

--- cogs/events/bot_events.py
+++ cogs/events/bot_events.py
@@ -90,6 +90,23 @@ class BotEvents(commands.Cog):
             ctx.author = message.author
         await self.bot.invoke(ctx)
 
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        log = self.bot.get_guild(675889958262931488).get_channel(697240163050586203)
+        embed = discord.Embed(color=self.bot.color)
+        embed.title = 'Novo servidor!'
+        embed.description = f'Nome: `{guild.name}`\nId: `{guild.id}`\nDono: `{guild.owner}`\nRegiao: `{guild.region}`'
+        await log.send(embed=embed)
+        await self.bot.db.new_guild(guild.id)
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        log = self.bot.get_guild(675889958262931488).get_channel(697240163050586203)
+        embed = discord.Embed(color=self.bot.ecolor)
+        embed.title = 'Removido de um servidor!'
+        embed.description = f'Nome: `{guild.name}`\nId: `{guild.id}`\nDono: `{guild.owner}`'
+        await log.send(embed=embed)
+        await self.bot.db.delete_guild(guild.id)
 
 def setup(bot):
     bot.add_cog(BotEvents(bot))
