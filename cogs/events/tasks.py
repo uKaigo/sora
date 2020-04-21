d332e11d546d3836c1bf9e729c64b80fb7126f25
uKaigo <37600692+uKaigo@users.noreply.github.com>
2020-04-21 18:09:13 -0300

    Change presences

--- cogs/events/tasks.py
+++ cogs/events/tasks.py
@@ -10,14 +10,14 @@ class Tasks(commands.Cog):
 
         # Todas as presenças e o número delas.
         self.presences = cycle([
-            (1, 'minha ajuda'),
-            (2, 'seus comandos'),
-            (3, 'o youtube')
+            (2, 'so.help'),
+            (1, 'meus comandos'),
+            (1, 'prefixo so.') 
         ])
 
         self.ch_presence.start()
 
-    @tasks.loop(seconds=120)
+    @tasks.loop(minutes=2)
     async def ch_presence(self):
         tp, name = next(self.presences)
         await self.bot.change_presence(activity=discord.Activity(name=name, type=tp, url='https://twitch.tv/ukaigo'))
