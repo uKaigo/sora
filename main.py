8ebc1ffe96213f66b016bbe9c35d4ac20d068249
uKaigo <37600692+uKaigo@users.noreply.github.com>
2020-06-24 00:43:46 -0300

    Definir logger para WARNING

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 
 # Configurando o logger
 logger = logging.getLogger('discord')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARNING)
 handler = logging.FileHandler(filename='sorabot.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
