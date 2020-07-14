f1057e12131a18d44ccb173d3bb06fd8f0114b42
uKaigo <37600692+uKaigo@users.noreply.github.com>
2020-07-14 00:44:39 -0300

    Suporte para videos no meme

--- cogs/commands/fun.py
+++ cogs/commands/fun.py
@@ -1,7 +1,9 @@
+from io import BytesIO
 from bs4 import BeautifulSoup
 from random import choice
+from re import sub
+from discord import File
 from discord.ext import commands
-
 
 class Fun(commands.Cog, name='_fun_cog'):
     def __init__(self, bot):
@@ -12,24 +14,38 @@ class Fun(commands.Cog, name='_fun_cog'):
         server = 'br.' if ctx.lang == 'pt-br' else ''
 
         async with ctx.typing():
-            res = await self.bot.session.get(f'https://{server}ifunny.co/page2')
+            res = await self.bot.session.get(f'https://{server}ifunny.co/')
 
             soup = BeautifulSoup(await res.text(), 'html.parser')
             
-            to_choice = soup.find_all(class_='media__preview')
-            to_choice = [a for a in to_choice if not a.attrs['href'].startswith('/video/')]
-            to_choice = [c.find('img') for c in to_choice]
-
+            to_choice = soup.find_all(class_='post__media')
+            
             if not to_choice:
                 embed = self.bot.erEmbed(ctx, ctx.t('err_notfound'))
                 embed.description = ctx.t('notfound_desc')
                 return await ctx.send(embed=embed)
+            
+            choosen = list(choice(to_choice).children)[1]
 
-            meme = choice(to_choice)
+            # É um video/gif
+            if choosen.attrs.get('data-type') and \
+                    (ext := choosen.attrs['data-type']) in ['image', 'video']:
+                ext = {'image': 'gif', 'video': 'mp4'}.get(ext) 
+                
+                url = 'https://ifunny.co'+list(list(choosen.children)[1].children)[0].attrs['href']
+                url = sub('\?gallery\=[\w\d]+', '', url)
 
-            embed = self.bot.embed(ctx)
-            embed.set_image(url=meme.attrs['data-src'])
-            await ctx.send(embed=embed)
+                to_send = await self.bot.session.get(choosen.attrs['data-source'])
+                io = BytesIO(await to_send.read())
+                await ctx.send(f'Link: <{url}>', file=File(io, filename=f'meme.{ext}'))
+            else: # É uma imagem
+                url = 'https://ifunny.co'+list(list(choosen.children)[1].children)[1].attrs['href']
+                url = sub('\?gallery\=[\w\d]+', '', url)
+
+                _surl = list(list(list(choosen.children)[1].children)[1].children)[0].attrs['data-src']
+                to_send = await self.bot.session.get(_surl)
+                io = BytesIO(await to_send.read())
+                await ctx.send(f'Link: <{url}>', file=File(io, filename=f'meme.jpg'))
 
 def setup(bot):
     bot.add_cog(Fun(bot))
